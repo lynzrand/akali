1b260a799cabb3193502ffdd1a0a8fa6e3f0e2a0
Rynco Li <lynzrand@outlook.com>
2018-12-06 16:14:29 +0800

    ADD fs

--- lib/api/fs.dart
+++ lib/api/fs.dart
@@ -1,3 +1,29 @@
 import 'dart:io';
+import 'dart:async';
 
-class AkaliFileManager {}
+abstract class AkaliFileManager {
+  FutureOr<FileManagementResponse> saveFile(
+      Stream<List<int>> file, String path);
+}
+
+class FileManagementResponse {
+  bool success;
+}
+
+class AkaliLocalFileManager implements AkaliFileManager {
+  final String rootPath;
+
+  AkaliLocalFileManager(this.rootPath);
+
+  @override
+  Future<FileManagementResponse> saveFile(
+    Stream<List<int>> file,
+    String path,
+  ) async {
+    var f = File(rootPath + path);
+    var writeSink = f.openWrite();
+    await file.pipe(writeSink);
+    writeSink.close();
+    return FileManagementResponse()..success = true;
+  }
+}
