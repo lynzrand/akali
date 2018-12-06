c3cf71e7dfa806fc3ec06756c40a036e2be0a584
Lynz Rand <plok314159265@163.com>
2018-12-06 15:09:04 +0800

    Digged pit for file uploading

--- lib/api/fs.dart
+++ lib/api/fs.dart
@@ -1,3 +1,21 @@
 import 'dart:io';
+import 'dart:async';
 
-class AkaliFileManager {}
+abstract class AkaliFileManager {
+  Future<String> streamFileFrom(String path, Stream<List<int>> fileStream);
+}
+
+class AkaliLocalFileManager implements AkaliFileManager {
+  final String rootPath;
+
+  AkaliLocalFileManager(this.rootPath);
+
+  @override
+  Future<String> streamFileFrom(
+      String path, Stream<List<int>> fileStream) async {
+    var fileToWrite = File(rootPath + path);
+    var writeSink = fileToWrite.openWrite();
+    await fileStream.pipe(writeSink);
+    return path;
+  }
+}
